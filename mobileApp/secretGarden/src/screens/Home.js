715116713670c8365d9b7b8f9ec69d46cf462cde
Francesco Battista <francesco.battista@finconsgroup.com>
2020-10-27 22:18:49 +0100

    Screen 1

--- mobileApp/secretGarden/src/screens/Home.js
+++ mobileApp/secretGarden/src/screens/Home.js
@@ -1,3 +1,11 @@
 import React from 'react'
 import {} from 'react-native'
+import {View} from "react-native-reanimated";
 
+const Home = () => {
+    return(
+        <View>
+    )
+}
+
+export default Home
